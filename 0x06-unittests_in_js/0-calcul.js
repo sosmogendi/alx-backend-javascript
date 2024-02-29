2051d8955ccc32a1024ab44e137b32a6e9d43833
Sospeter <sosmogendi@gmail.com>
2024-02-29 19:52:32 +0000

    Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+// Calculation function.
+
+function calculateNumber(a, b) {
+    fstInt = Math.round(a);
+    scndInt = Math.round(b);
+    return fstInt + scndInt;
+}
+
+module.exports = calculateNumber;
