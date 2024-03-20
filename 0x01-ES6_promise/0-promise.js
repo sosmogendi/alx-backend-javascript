5491f2f47f4607b236c80526aa73ca3303a275e0
Sospeter <sosmogendi@gmail.com>
2024-03-20 18:09:35 +0000

    Return a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,9 @@
-// 0-promise.js
-
 /**
  * Returns a Promise that resolves after a short delay.
  * @returns {Promise} A Promise that resolves after a short delay.
  */
 export default function getResponseFromAPI() {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     // Simulate an asynchronous API call with a delay of 1 second
     setTimeout(() => {
       resolve('API response');
